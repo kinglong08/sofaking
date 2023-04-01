0d9023399a49d8ba604cc67247761fdc53172f0f
kinglong <daijl@sugon.com>
2023-04-01 15:09:04 +0800

    Initial commit

---
+++ src/main/java/com/sofaking/sofaking/SofakingApplication.java
@@ -0,0 +1,13 @@
+package com.sofaking.sofaking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SofakingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SofakingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sofaking/sofaking/SofakingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofaking.sofaking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SofakingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
