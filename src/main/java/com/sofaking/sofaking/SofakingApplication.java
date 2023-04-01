a5ef7a87013ce54ae923fd7529b3bdc824ffb503
kinglong <daijl@sugon.com>
2023-04-01 15:10:29 +0800

    init

--- src/main/java/com/sofaking/sofaking/SofakingApplication.java
+++ src/main/java/com/sofaking/sofaking/SofakingApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SofakingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SofakingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SofakingApplication.class, args);
+    }
 
 }
--- src/test/java/com/sofaking/sofaking/SofakingApplicationTests.java
+++ src/test/java/com/sofaking/sofaking/SofakingApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SofakingApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
